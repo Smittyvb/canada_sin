b363ec9f31214c185ff879c8dc699466cedbb9df
Smitty <smitop2@gmail.com>
2020-09-25 11:53:41 -0400

    improve SINType

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 use std::convert::TryInto;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[non_exhaustive]
 /// An error resulting from parsing a SIN
 pub enum SINParseError {
     TooLong,
@@ -12,8 +13,10 @@ pub enum SINParseError {
 
 /// Types of SINs: All the provinces, plus some other categories.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[non_exhaustive]
 pub enum SINType {
-    /// CRA-assigned Individual Tax Numbers, Temporary Tax Numbers and Adoption Tax Numbers
+    /// CRA-assigned Individual Tax Numbers, Temporary Tax Numbers and Adoption Tax Numbers. These
+    /// are currently only assigned to natural people.
     CRAAssigned,
     TemporaryResident,
     /// Business numbers and SINs share the same namespace.
@@ -35,9 +38,36 @@ pub enum SINType {
     Yukon,
 }
 
+impl SINType {
+    /// Does the SIN repersent someone in a province?
+    pub fn is_province(self) -> bool {
+        use SINType::*;
+        matches!(
+            self,
+            Alberta
+                | BritishColumbia
+                | Manitoba
+                | NewBrunswick
+                | NewfoundlandLabrador
+                | NorthwestTerritories
+                | NovaScotia
+                | Nunavut
+                | Ontario
+                | PrinceEdwardIsland
+                | Quebec
+                | Saskatchewan
+                | Yukon
+        )
+    }
+    /// Does the SIN repersent a human? Currently only business numbers are assigned to non-humans.
+    pub fn is_human(self) -> bool {
+        !matches!(self, Self::BusinessNumber)
+    }
+}
+
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub struct SIN {
-    inner_digits: [u8; 9]
+    inner_digits: [u8; 9],
 }
 
 impl SIN {
@@ -47,27 +77,30 @@ impl SIN {
             if let Some(digit) = khar.to_digit(10) {
                 digits.push(digit as u8);
             };
-        };
+        }
         match digits.len() {
             n if n < 9 => return Err(SINParseError::TooShort),
             n if n > 9 => return Err(SINParseError::TooLong),
             9 => {
                 // luhn checksum
-                let luhn_sum: u8 = digits.iter()
+                let luhn_sum: u8 = digits
+                    .iter()
                     .enumerate()
                     .map(|(idx, digit)| digit * (if idx % 2 == 0 { 1u8 } else { 2u8 }))
-                    .map(|val| if val > 9 {
-                        // since 16 turns into 1 + 6, and the max value we will se here is 18,
-                        // this will always give the right value
-                        (val % 10) + 1
-                    } else {
-                        val
+                    .map(|val| {
+                        if val > 9 {
+                            // since 16 turns into 1 + 6, and the max value we will se here is 18,
+                            // this will always give the right value
+                            (val % 10) + 1
+                        } else {
+                            val
+                        }
                     })
                     .sum();
                 if dbg!(luhn_sum) % 10 != 0 {
                     return Err(SINParseError::InvalidChecksum);
                 }
-            },
+            }
             _ => unreachable!(),
         };
         let boxed_digits = digits.into_boxed_slice();
@@ -84,16 +117,28 @@ impl SIN {
     /// - `TemporaryResident` (starts with 9)
     /// - `Quebec` (starts with 2 or 3)
     /// - `BusinessNumber` sometimes (if it starts with 8 it's a business number, if it starts with 7 it *might* be one)
-    /// 
+    ///
     /// The logic is based on [this mapping](https://en.wikipedia.org/wiki/Social_Insurance_Number#Geography).
     pub fn types(&self) -> Vec<SINType> {
         use SINType::*;
         match self.inner_digits[0] {
             0 => vec![CRAAssigned],
-            1 => vec![NovaScotia, NewBrunswick, PrinceEdwardIsland, NewfoundlandLabrador],
+            1 => vec![
+                NovaScotia,
+                NewBrunswick,
+                PrinceEdwardIsland,
+                NewfoundlandLabrador,
+            ],
             2 | 3 => vec![Quebec],
             4 | 5 => vec![Ontario, OverseasForces],
-            6 => vec![Ontario, Manitoba, Saskatchewan, Alberta, NorthwestTerritories, Nunavut],
+            6 => vec![
+                Ontario,
+                Manitoba,
+                Saskatchewan,
+                Alberta,
+                NorthwestTerritories,
+                Nunavut,
+            ],
             7 => vec![BritishColumbia, Yukon, BusinessNumber],
             8 => vec![BusinessNumber],
             9 => vec![TemporaryResident],
@@ -108,19 +153,37 @@ mod tests {
 
     #[test]
     fn parse_sin_checks_luhn() {
-        assert_eq!(SIN::parse("123456789".to_string()), Err(SINParseError::InvalidChecksum));
-        assert_eq!(SIN::parse("425453457".to_string()), Err(SINParseError::InvalidChecksum));
-        assert_eq!(SIN::parse("759268676".to_string()), Err(SINParseError::InvalidChecksum));
-        assert_eq!(SIN::parse("635563453".to_string()), Err(SINParseError::InvalidChecksum));
+        assert_eq!(
+            SIN::parse("123456789".to_string()),
+            Err(SINParseError::InvalidChecksum)
+        );
+        assert_eq!(
+            SIN::parse("425453457".to_string()),
+            Err(SINParseError::InvalidChecksum)
+        );
+        assert_eq!(
+            SIN::parse("759268676".to_string()),
+            Err(SINParseError::InvalidChecksum)
+        );
+        assert_eq!(
+            SIN::parse("635563453".to_string()),
+            Err(SINParseError::InvalidChecksum)
+        );
         // make sure this doesn't cause an overflow
-        assert_eq!(SIN::parse("999999999".to_string()), Err(SINParseError::InvalidChecksum));
+        assert_eq!(
+            SIN::parse("999999999".to_string()),
+            Err(SINParseError::InvalidChecksum)
+        );
         assert!(SIN::parse("046454286".to_string()).is_ok());
         assert!(SIN::parse("000000000".to_string()).is_ok());
     }
 
     #[test]
     fn parse_sin_checks_too_short() {
-        assert_eq!(SIN::parse("12345678".to_string()), Err(SINParseError::TooShort));
+        assert_eq!(
+            SIN::parse("12345678".to_string()),
+            Err(SINParseError::TooShort)
+        );
         assert_eq!(SIN::parse("123".to_string()), Err(SINParseError::TooShort));
         assert_eq!(SIN::parse("0".to_string()), Err(SINParseError::TooShort));
         assert_eq!(SIN::parse("".to_string()), Err(SINParseError::TooShort));
@@ -128,11 +191,29 @@ mod tests {
 
     #[test]
     fn parse_sin_checks_too_long() {
-        assert_eq!(SIN::parse("0000000000".to_string()), Err(SINParseError::TooLong));
-        assert_eq!(SIN::parse("4324234237".to_string()), Err(SINParseError::TooLong));
-        assert_eq!(SIN::parse("635462452452344343".to_string()), Err(SINParseError::TooLong));
-        assert_eq!(SIN::parse("999999999999999999999999999".to_string()), Err(SINParseError::TooLong));
-        assert_eq!(SIN::parse("000000000000000000000000000".to_string()), Err(SINParseError::TooLong));
-        assert_eq!(SIN::parse("543537672346234345464254235".to_string()), Err(SINParseError::TooLong));
+        assert_eq!(
+            SIN::parse("0000000000".to_string()),
+            Err(SINParseError::TooLong)
+        );
+        assert_eq!(
+            SIN::parse("4324234237".to_string()),
+            Err(SINParseError::TooLong)
+        );
+        assert_eq!(
+            SIN::parse("635462452452344343".to_string()),
+            Err(SINParseError::TooLong)
+        );
+        assert_eq!(
+            SIN::parse("999999999999999999999999999".to_string()),
+            Err(SINParseError::TooLong)
+        );
+        assert_eq!(
+            SIN::parse("000000000000000000000000000".to_string()),
+            Err(SINParseError::TooLong)
+        );
+        assert_eq!(
+            SIN::parse("543537672346234345464254235".to_string()),
+            Err(SINParseError::TooLong)
+        );
     }
 }
